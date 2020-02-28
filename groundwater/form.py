fce8c52f285956acde9cad35552d85c4e3a3f24d
veda1234 <vedaantibaliga@gmail.com>
2020-02-28 13:58:49 +0530

    Update form.py

--- groundwater/form.py
+++ groundwater/form.py
@@ -59,6 +59,24 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose another one')
+                
+                
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+            
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
 
 
 class UserDatabaseForm(FlaskForm):
