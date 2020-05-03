4c9b16299e89879f1207ff05a85682d45aaf3282
Vedaanti Baliga <vedaantibaliga@gmail.com>
2020-05-03 16:38:42 +0530

    Update config.py

--- groundwater/config.py
+++ groundwater/config.py
@@ -1,4 +1,8 @@
 import os
+import json
+
+with open('/etc/config.json') as config_file:  # make config.json file in Linux system with environment variables for secret key and database URI
+	config = json.load(config_file)
 
 class Config:
     SECRET_KEY = 'd168651a2aa242e14428a991c42164ef'
