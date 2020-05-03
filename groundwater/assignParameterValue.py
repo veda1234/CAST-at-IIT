11fc3e173137721a6f8d807f44400ac9cb15fa06
Vedaanti Baliga <vedaantibaliga@gmail.com>
2020-05-03 21:03:16 +0530

    Update assignParameterValue.py

--- groundwater/assignParameterValue.py
+++ groundwater/assignParameterValue.py
@@ -6,6 +6,6 @@ def assign_parameter_value(parameter):
         index = 4
     elif parameter == 'Electron Donor[mg/l]':
         index = 7
-    elif parameter == 'Electron Acceptor : O2[mg/l]':
+    elif parameter == 'Electron Acceptors : O2[mg/l]':
         index = 8
     return index
